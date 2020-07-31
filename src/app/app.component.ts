9bb76196efd781d08792d71cb8003043d3e87ad1
renamoo <renamoo808@gmail.com>
2020-07-31 23:51:40 +0900

    #3 picture element demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,9 +31,20 @@ export class AppComponent {
     { id: 'dachshund', src: 'assets/dachshund.jpg' },
     { id: 'shiba', src: 'assets/shiba.jpg' }
   ];
+  imagesWithDirection: CarouselImage[] = [
+    {
+      id: 'australian', src: 'assets/australian.jpg',
+      sources: [
+        { srcset: 'assets/australian.webp', type: 'image/webp' },
+      ]
+    },
+    { id: 'dachshund', src: 'assets/dachshund.jpg' },
+    { id: 'shiba', src: 'assets/shiba.jpg' }
+  ];
   controllerTop = new CarouselController();
   controllerButton = new CarouselController();
   controllerDots = new CarouselController();
+  controllerDirection = new CarouselController();
   controllerAuto = new CarouselController();
   usages = USAGES;
   apis = APIS;
@@ -90,6 +101,39 @@ const USAGES = [
 
     images: CarouselImage[] = [
       { id: 'australian', src: 'assets/australian.jpg' },
+      { id: 'dachshund', src: 'assets/dachshund.jpg' },
+      { id: 'shiba', src: 'assets/shiba.jpg' }
+    ];
+
+    onImgChange(id: string) {
+      console.log(id);
+    }
+}
+    `
+  },
+  {
+    id: 'artDirection',
+    title: 'Art Direction (Image Optimization)',
+    html: `<div class="wrap_carousel">
+    <ng-simple-carousel-with-dots [images]="images" [controller]="controller" (imgChange)="onImgChange($event)">
+    </ng-simple-carousel-with-dots>
+</div>`,
+    css: `.wrap_carousel {
+      margin:auto;
+      // image aspect ratio is 3 : 2
+      width:80%;
+      height: calc((50vw - 40px) * 0.8 / 3 * 2);
+    }`,
+    ts: `export class SampleComponent {
+    controller = new CarouselController();
+
+    images: CarouselImage[] = [
+      {
+        id: 'australian', src: 'assets/australian.jpg',
+        sources: [
+          { srcset: 'assets/australian.webp', type: 'image/webp' },
+        ]
+      },
       { id: 'dachshund', src: 'assets/dachshund.jpg' },
       { id: 'shiba', src: 'assets/shiba.jpg' }
     ];
